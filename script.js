60e7353b28289b8cc5ddcde3c44583d7a4577d03
d-shah-09 <shahdrishti9607@gmail.com>
2026-09-08 23:03:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// // Smooth scrolling for the simple navigation
+// const links = document.querySelectorAll("nav a[href^='#']");
+
+// links.forEach(link => {
+//     link.addEventListener("click", function (event) {
+//         event.preventDefault();
+
+//         const section = document.querySelector(this.getAttribute("href"));
+
+//         if (section) {
+//             section.scrollIntoView({
+//                 behavior: "smooth"
+//             });
+//         }
+//     });
+// });
